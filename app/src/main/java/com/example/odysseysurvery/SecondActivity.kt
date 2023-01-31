9fc2d9eb7dcc089172eef2acda3b5a0a23e9209c
aryanteng <aryanteng892@gmail.com>
2023-02-01 01:20:44 +0530

    clear operation and intent with array sent

--- app/src/main/java/com/example/odysseysurvery/SecondActivity.kt
+++ app/src/main/java/com/example/odysseysurvery/SecondActivity.kt
@@ -1,16 +1,81 @@
 package com.example.odysseysurvery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Toast
+import java.util.Dictionary
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var btnSubmit: Button
     private lateinit var btnClear: Button
+    private lateinit var cbMusic: CheckBox
+    private lateinit var cbDance: CheckBox
+    private lateinit var cbPlay: CheckBox
+    private lateinit var cbFashion: CheckBox
+    private lateinit var cbFood: CheckBox
+    private val arr: ArrayList<String> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        btnSubmit = findViewById(R.id.btn_submit)
+        btnClear = findViewById(R.id.btn_clear)
+        cbMusic = findViewById(R.id.cb_music)
+        cbDance = findViewById(R.id.cb_dance)
+        cbPlay = findViewById(R.id.cb_play)
+        cbFashion = findViewById(R.id.cb_fashion)
+        cbFood = findViewById(R.id.cb_food)
+
+        btnClear.setOnClickListener{
+            if(cbMusic.isChecked){
+                cbMusic.toggle()
+            }
+            if(cbDance.isChecked){
+                cbDance.toggle()
+            }
+            if(cbPlay.isChecked){
+                cbPlay.toggle()
+            }
+            if(cbFashion.isChecked){
+                cbFashion.toggle()
+            }
+            if(cbFood.isChecked){
+                cbFood.toggle()
+            }
+        }
+
+        btnSubmit.setOnClickListener{
+            arr.clear()
+            if(cbMusic.isChecked){
+                arr.add("music")
+            }
+            if(cbDance.isChecked){
+                arr.add("dance")
+            }
+            if(cbPlay.isChecked){
+                arr.add("play")
+            }
+            if(cbFashion.isChecked){
+                arr.add("fashion")
+            }
+            if(cbFood.isChecked){
+                arr.add("food")
+            }
+            Log.d("tag", arr.toString())
+            Log.d("tag2", arr.javaClass.toString())
+            Toast.makeText(this, "Entry Recorded!", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ThirdActivity::class.java)
+            intent.putExtra("map", arr)
+            startActivity(intent)
+        }
     }
+
+
 
 
 
--- app/src/main/java/com/example/odysseysurvery/ThirdActivity.kt
+++ app/src/main/java/com/example/odysseysurvery/ThirdActivity.kt
@@ -1,11 +1,19 @@
 package com.example.odysseysurvery
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class ThirdActivity : AppCompatActivity() {
+    private var arr: ArrayList<String> = arrayListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+
+        arr = intent.getStringArrayListExtra("map") as ArrayList<String>
+        Log.d("third", arr.toString())
+
+
     }
+
 }
