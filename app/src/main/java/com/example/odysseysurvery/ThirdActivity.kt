a23c89339ad1efc9b79b2c9077dc5cbecb9cf6ff
aryanteng <aryanteng892@gmail.com>
2023-02-01 02:37:47 +0530

    third activity working with name and role

--- app/src/main/java/com/example/odysseysurvery/ThirdActivity.kt
+++ app/src/main/java/com/example/odysseysurvery/ThirdActivity.kt
@@ -2,19 +2,31 @@ package com.example.odysseysurvery
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ThirdActivity : AppCompatActivity() {
     private lateinit var name: String
     private lateinit var role: String
+    private lateinit var tvName: TextView
+    private lateinit var tvRole: TextView
     private var arr: ArrayList<String> = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
 
-        name= intent.getStringExtra("name").toString()
-        role= intent.getStringExtra("role").toString()
-        arr = intent.getStringArrayListExtra("map") as ArrayList<String>
+        tvName = findViewById(R.id.tv_name)
+        tvRole = findViewById(R.id.tv_role)
+
+        name = intent.getStringExtra("name").toString()
+        role = intent.getStringExtra("role").toString()
+        arr  = intent.getStringArrayListExtra("map") as ArrayList<String>
+
+        tvName.text = name
+        tvRole.text = role
+
+
         Log.d("third", arr.toString())
 
 
