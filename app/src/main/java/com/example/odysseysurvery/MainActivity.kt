c5b2ef79c8d123baca3e7b12d02123625c92aaab
aryanteng <aryanteng892@gmail.com>
2023-01-31 23:11:58 +0530

    second activity checkboxes completed

--- app/src/main/java/com/example/odysseysurvery/MainActivity.kt
+++ app/src/main/java/com/example/odysseysurvery/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.odysseysurvery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnNext: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnNext = findViewById(R.id.btn_next)
+
+        btnNext.setOnClickListener{
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
